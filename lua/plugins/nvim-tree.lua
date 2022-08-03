37e1046b745c7503c6591c6012c9b0558a26cad6
tinnguyentg <tinnguyen121221@gmail.com>
2022-08-03 10:22:51 +0700

    uncomment nvim-tree mapping

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,11 +3,11 @@
 --]]
 
 return function ()
-	-- local map = require("maps").map
+	local map = require("maps").map
 	require("nvim-tree").setup({
 		filters = { custom = { "^.git$" } }
 	})
 
-	-- map('n', '<C-b>', ':NvimTreeToggle<CR>')
-	-- map('i', '<C-b>', '<ESC>:NvimTreeToggle<CR>')
+	map('n', '<C-b>', ':NvimTreeToggle<CR>')
+	map('i', '<C-b>', '<ESC>:NvimTreeToggle<CR>')
 end
