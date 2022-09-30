d503340ba2ea5dbdbc5499df44bbc675a0404da3
tinnguyentg <tinnguyen121221@gmail.com>
2022-09-30 14:18:32 +0700

    use pypsl

---
+++ lua/plugins/lsp/pylsp.lua
@@ -0,0 +1,19 @@
+local lspconfig = require("lspconfig")
+local config = require("plugins.lsp.config")
+
+lspconfig.pylsp.setup({
+	on_attach = config.on_attach,
+	capabilities = config.capabilities(),
+	settings = {
+		pylsp = {
+			plugins = {
+				pycodestyle = {
+					enabled = false,
+				},
+				flake8 = {
+					enabled = true,
+				},
+			},
+		},
+	},
+})
--- lua/variables.lua
+++ lua/variables.lua
@@ -8,7 +8,8 @@ vim.g.mason_tool_installer_ensured_installed = {
 -- Mason lsp servers map
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 vim.g.mason_lsp_ensured_installed = {
-	"pyright",
+	-- "pyright",
+	"pylsp",
 	"tsserver",
 	"intelephense",
 	"volar",
