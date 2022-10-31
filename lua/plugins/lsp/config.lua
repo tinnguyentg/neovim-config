57ed45661268a84f5c029e5b86666a47beeb1269
tinnguyentg <tinnguyen121221@gmail.com>
2022-10-31 21:09:28 +0700

    .

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -26,7 +26,7 @@ end
 
 function M.capabilities()
 	require("plugins.cmp")()
-	local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+	local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 	return capabilities
 end
 
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -5,15 +5,6 @@
 return function()
 	local null_ls = require("null-ls")
 	local sources = {
-		null_ls.builtins.formatting.pint,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.isort,
-		null_ls.builtins.formatting.xmllint,
-		null_ls.builtins.formatting.shellharden,
-
-		null_ls.builtins.code_actions.shellcheck,
 	}
 
 	null_ls.setup({
