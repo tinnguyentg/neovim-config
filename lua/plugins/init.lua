0c7ef53100a1fd792bc840c370566bc0bb0c1c24
tinnguyentg <tinnguyen121221@gmail.com>
2022-11-08 16:27:23 +0700

    .

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,7 +24,7 @@ return require("packer").startup(function(use)
 
 	-- Nvim-tree
 	use({
-		"kyazdani42/nvim-tree.lua",
+		"nvim-tree/nvim-tree.lua",
 		config = require("plugins.nvim-tree"),
 	})
 	use("airblade/vim-gitgutter")
